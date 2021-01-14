db6f4ee70f012ef6a35a905015fa619ebbc788d4
ToyBotDroid <nischan127@gmail.com>
2021-01-14 17:35:22 +0600

    finally works !

--- game.js
+++ game.js
@@ -7,15 +7,26 @@ const playerChosen = document.querySelector('#playerChosen')
 const computerChosen = document.querySelector('#computerChosen')
 
 const displayResult = document.querySelector('#result')
-const result = whoWon()
+let result 
 
 
 // Player Selection and the evaluation
 
 choices.forEach(choice => {
   choice.addEventListener('click', (e) =>{
+
+// Computer Random output
+
+const option = ["rock", "paper", "scissors"]
+const random = Math.round(Math.random()*(option.length - 1));
+
+function computerOutput(){
+  return option[random]
+}
+  
+
   playerMove = e.target.id
-  computerOutput()
+  computerMove = computerOutput()
   whoWon()
   playerChosen.innerHTML = playerMove
   computerChosen.innerHTML = computerMove
@@ -23,38 +34,31 @@ choices.forEach(choice => {
 
   console.log(computerChosen)
   console.log(playerChosen)
+  console.log(computerOutput())
   })
 })
 
-// Computer Random output
-
-const option = ["rock", "paper", "scissors"]
-const random = Math.round(Math.random()*(2));
-
-function computerOutput(){
-  return option[random]
-}
 
 // Decide winner
 
 function whoWon(){
   if (computerMove === playerMove){
-    return "It's a tie";
+    return result = "It's a tie";
   } else if ( computerMove === "rock" && playerMove === "paper"){
-    return "Player 1 won !";
+    return result = "Player 1 won !";
   } else if ( computerMove === "rock" && playerMove === "scissors"){
-    return "Computer won !";
+    return result = "Computer won !";
   } else if ( computerMove === "paper" && playerMove === "scissors"){
-    return "Player 1 won !";
+    return result = "Player 1 won !";
   } else if ( computerMove === "paper" && playerMove === "rock"){
-    return "Computer won !";
+    return result = "Computer won !";
   } else if ( computerMove === "scissors" && playerMove === "rock"){
-    return "Player 1 won !";
+    return result = "Player 1 won !";
   } else if ( computerMove === "scissors" && playerMove === "paper"){
-    return "computer won !";
+    return result = "Computer won !";
   }
 }
 
-console.log(computerOutput())
+
 
 
