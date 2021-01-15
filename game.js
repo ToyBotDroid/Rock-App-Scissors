f71a0a4f395c0d06f558164e7de0dc316d0e8c30
ToyBotDroid <nischan127@gmail.com>
2021-01-15 13:34:52 +0600

    scoring system and restart

--- game.js
+++ game.js
@@ -1,13 +1,15 @@
 let playerMove
 let  computerMove
-
 const choices = document.querySelectorAll('.choices')
-
 const playerChosen = document.querySelector('#playerChosen')
 const computerChosen = document.querySelector('#computerChosen')
-
 const displayResult = document.querySelector('#result')
 let result 
+var playerScore = 0
+var computerScore = 0
+const displayPlayerScore = document.querySelector('.playerScore')
+const displayComputerScore = document.querySelector('.computerScore')
+
 
 
 // Player Selection and the evaluation
@@ -27,38 +29,75 @@ function computerOutput(){
 
   playerMove = e.target.id
   computerMove = computerOutput()
-  whoWon()
+  play()
+  updateScore()
   playerChosen.innerHTML = playerMove
   computerChosen.innerHTML = computerMove
   displayResult.innerHTML = result
 
-  console.log(computerChosen)
-  console.log(playerChosen)
-  console.log(computerOutput())
+  displayPlayerScore.innerHTML = `<b> Player Score = ${playerScore} </b>` 
+  displayComputerScore.innerHTML = `<b> Computer Score = ${computerScore} </b>`
+
+  // console.log(computerChosen)
+  // console.log(playerChosen)
+  // console.log(computerOutput())
+  console.log(playerScore)
+  console.log(computerScore)
   })
 })
 
 
-// Decide winner
+// Play function 
 
-function whoWon(){
+function play(){
   if (computerMove === playerMove){
     return result = "It's a tie";
   } else if ( computerMove === "rock" && playerMove === "paper"){
-    return result = "Player 1 won !";
+    return result = "Player won !";
+    // return playerScore++;
   } else if ( computerMove === "rock" && playerMove === "scissors"){
     return result = "Computer won !";
+    // computerScore++;
   } else if ( computerMove === "paper" && playerMove === "scissors"){
-    return result = "Player 1 won !";
+    return result = "Player won !";
+    // return playerScore++;
   } else if ( computerMove === "paper" && playerMove === "rock"){
     return result = "Computer won !";
+    // computerScore++;
   } else if ( computerMove === "scissors" && playerMove === "rock"){
-    return result = "Player 1 won !";
+    return result = "Player won !";
+    // return playerScore++;
   } else if ( computerMove === "scissors" && playerMove === "paper"){
     return result = "Computer won !";
+    // computerScore++;
   }
 }
 
+// because it doesn't work if i do it on the above function
 
+function updateScore(){
+  
+  if ( computerMove === "rock" && playerMove === "paper"){
+    return playerScore++;
+  } else if ( computerMove === "rock" && playerMove === "scissors"){
+    computerScore++;
+  } else if ( computerMove === "paper" && playerMove === "scissors"){
+    return playerScore++;
+  } else if ( computerMove === "paper" && playerMove === "rock"){
+    computerScore++;
+  } else if ( computerMove === "scissors" && playerMove === "rock"){
+    return playerScore++;
+  } else if ( computerMove === "scissors" && playerMove === "paper"){
+    computerScore++;
+  }
+}
 
+let restart = document.querySelector('.restart')
 
+restart.addEventListener('click', () => {
+  playerScore = 0
+  computerScore =0
+
+  displayPlayerScore.innerHTML = `<b> Player Score = ${playerScore} </b>` 
+  displayComputerScore.innerHTML = `<b> Computer Score = ${computerScore} </b>`
+})
